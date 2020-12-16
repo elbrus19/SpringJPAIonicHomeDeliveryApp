7b1f52c1a0bf53ba792d9f9358af0887a3480930
elbrus19 <elbrumen14@gmail.com>
2020-12-16 13:06:17 +0000

    advance project continue

--- frontend/HomeDeliveryApp/src/app/maps/maps.page.ts
+++ frontend/HomeDeliveryApp/src/app/maps/maps.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as M from 'leaflet';
 
 @Component({
   selector: 'app-maps',
@@ -10,6 +11,25 @@ export class MapsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+    var map = M.map('map').setView([28.140811, -15.4921491], 10.5);
+    var popup = M.popup();
 
+    M.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1Ijoic2VyZ2lvcGExMSIsImEiOiJja2libnFvZzIxMTR6MnN0Z3ZvMXU3eGU5In0.2XrcyC4TpekDLFY-pKom8Q'
+    }).addTo(map);
+
+    function onMapClick(e) {
+      popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng)
+        .openOn(map);
+    }
+
+    map.on('click', onMapClick);
+  }
 }
